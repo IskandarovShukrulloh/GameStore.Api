e729dc3f9f15785ad94006fb51d630f965b14c47
Shukrulloh <iskandarov.sh8228@gmail.com>
2025-08-06 22:28:28 +0500

    ignore

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -9,5 +9,14 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options)
 {
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
-    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new Genre { Id = 1, Name = "Action" },
+            new Genre { Id = 2, Name = "Adventure" },
+            new Genre { Id = 3, Name = "Role-Playing" },
+            new Genre { Id = 4, Name = "Simulation" },
+            new Genre { Id = 5, Name = "Strategy" }
+        );
+    }
 }
